46f1054266daf9033df7e1c1c48d967d2944cc96
Aamir Sahil <notifysahil@gmail.com>
2024-08-29 17:38:26 +0530

    crawler complete

--- crawler/page_crawler.py
+++ crawler/page_crawler.py
@@ -1,4 +1,5 @@
 import scrapy
+from bs4 import BeautifulSoup
 
 class BlogSpider(scrapy.Spider):
     name = 'naruto_page'
@@ -6,7 +7,7 @@ class BlogSpider(scrapy.Spider):
 
     def parse(self, response):
         for href in response.css('.smw-columnlist-container')[0].css('a::attr(href)').extract():
-            extracted_data = scrapy.Request('https://naruto.fandom.com/wiki/' + href,
+            extracted_data = scrapy.Request('https://naruto.fandom.com' + href,
                            callback=self._parse_page)
             yield extracted_data
 
@@ -14,11 +15,33 @@ class BlogSpider(scrapy.Spider):
             yield response.follow(next_page, self.parse)
 
     def _parse_page(self, response):
+        
+        title = ""
+        classification = ""
+        description = ""
+
         title = response.css("span.mw-page-title-main::text").extract()[0]
         title = title.strip()
 
         div_selector = response.css("div.mw-parser-output")[0]
         div_html = div_selector.extract()
+
+        soup = BeautifulSoup(div_html).find('div')
+
+        if soup.find('aside'):
+            aside = soup.find('aside')
+
+            for cell in aside.find_all('div', {'class' : 'pi-data'}):
+                if cell.find('h3'):
+                    cell_name = cell.find('h3').text.strip()
+                    if cell_name == "Classification":
+                        classification = cell.find('div').text.strip()
+            
+            soup.find('aside').decompose()
+            description = soup.text.strip()
+            description = description.split('Trivia')[0].strip()
         
-        classification = 
-        description = ""
+        return dict(
+            jutsu_name = title,
+            justsu_type = classification,
+            jutsu_description = description)
