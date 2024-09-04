d7dd481199e938050ed554f2f1e0e42e12b4aa76
Aamir Sahil <notifysahil@gmail.com>
2024-09-04 08:38:11 +0530

    gradio bar plot complete

--- gradio_app.py
+++ gradio_app.py
@@ -2,9 +2,28 @@ import gradio as gr
 from theme_classifier import ThemeClassifier
 
 def get_themes(theme_list_str, subtitle_path, save_path):
+    # run model to get themes
     theme_list = theme_list_str.split(',')
     theme_classifier = ThemeClassifier(theme_list)
     theme_df = theme_classifier.get_themes(subtitle_path, save_path)
+    
+    # find the sum of output themes
+    output_df = theme_df.drop(['episode', 'script'], axis=1).sum().reset_index()
+    output_df.columns = ['Theme', 'Score']
+
+    # create bar plot
+    output_chart = gr.BarPlot(
+        output_df,
+        x="Theme",
+        y="Score",
+        title="Series Theme",
+        tooltip=['Theme', 'Score'],
+        vertical=False,
+        width=500,
+        height=250
+    )
+
+    return output_chart
 
 
 
